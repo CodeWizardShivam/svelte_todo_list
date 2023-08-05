941e18179d7a993b0fc492ca5b6316b654e05886
Shivam Pandey <shivam-pandey@outlook.in>
2023-08-05 09:34:22 +0530

    Added Some Style And Bugs

--- src/lib/ToDoList.svelte
+++ src/lib/ToDoList.svelte
@@ -26,53 +26,89 @@
   }
 </script>
 
-{#if isLoading}
-  <p class="state-text">Loading...</p>
-{:else if error}
-  <p class="state-text">{error}</p>
-{:else if toDos}
-  <ul style="text-align: left;">
-    {#each toDos as toDo}
-      <li>
-        <slot {toDo}>
-          <label>
-            <input
-              on:input={(event) => {
-                event.currentTarget.checked = toDo.completed;
-                handleToggleTodo(toDo.id, !toDo.completed);
-              }}
-              type="checkbox"
-              checked={toDo.completed}
-            />
-            {toDo.title}
+<div class="ToDoList-wrapper">
+  <div class="todolist">
+    {#if isLoading}
+      <p class="state-text">Loading...</p>
+    {:else if error}
+      <p class="state-text">{error}</p>
+    {:else if toDos}
+      <ul style="text-align: left;">
+        {#each toDos as toDo}
+          <li>
+            <slot {toDo}>
+              <label>
+                <input
+                  on:input={(event) => {
+                    event.currentTarget.checked = toDo.completed;
+                    handleToggleTodo(toDo.id, !toDo.completed);
+                  }}
+                  type="checkbox"
+                  checked={toDo.completed}
+                />
+                {toDo.title}
 
-            <button
-              class="remove-todo-button"
-              aria-label={toDo.title}
-              on:click={() => handleRemove(toDo.id)}
-            >
-              <span style:width="10px" style:display = "inline-block"><FaRegTrashAlt /></span></button
-            >
-          </label>
-        </slot>
-      </li>
-    {/each}
-  </ul>
-{/if}
-<div >
-<form class="todo-add" action="" on:submit|preventDefault={handleAddTodo}>
-  <input bind:value={inp} />
-</form>
-<button class="todo-add"
-  on:click={() => {
-    handleAddTodo();
-  }}>Add</button
->
+                <button
+                  class="remove-todo-button"
+                  aria-label={toDo.title}
+                  on:click={() => handleRemove(toDo.id)}
+                >
+                  <span style:width="10px" style:display="inline-block"
+                    ><FaRegTrashAlt /></span
+                  ></button
+                >
+              </label>
+            </slot>
+          </li>
+        {/each}
+      </ul>
+    {/if}
+  </div>
+
+  <form class="todo-add" action="" on:submit|preventDefault={handleAddTodo}>
+    <input bind:value={inp} />
+  </form>
+  <button
+    class="todo-add"
+    style={"background-color:red; color:white;"}
+    on:click={() => {
+      handleAddTodo();
+    }}>Add</button
+  >
 </div>
 
-<style>
-  .todo-add{
+<style lang="scss">
+  .todo-add {
     display: inline;
-    
+
+    /* background-color: red; */
+  }
+
+  .ToDoList-wrapper {
+    background-color: #424242;
+    border: 1px solid #4b4b4b;
+  }
+  .todolist {
+    max-width: 400px;
+    ul {
+      margin: 0px;
+      padding: 10px;
+    }
+    li {
+      display: flex;
+      margin-bottom: 5px;
+      background-color: #303030;
+      padding: 10px;
+      lable {
+        input {
+          margin: 0 20 0 0;
+        }
+        cursor: progress;
+
+        display: flex;
+        align-items: baseline;
+        
+      }
+    }
   }
 </style>
